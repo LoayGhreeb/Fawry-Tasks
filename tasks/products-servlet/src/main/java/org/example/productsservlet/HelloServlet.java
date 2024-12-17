9be38de43418155ecc712656a6c0c64c0540a9a5
Loay Ghreeb <loayahmed655@gmail.com>
2024-12-17 04:10:47 +0200

    initialize new project

---
+++ tasks/products-servlet/src/main/java/org/example/productsservlet/HelloServlet.java
@@ -0,0 +1,25 @@
+package org.example.productsservlet;
+
+import java.io.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet("/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    @Override
+    public void init() {
+        message = "Hello World!";
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+}
